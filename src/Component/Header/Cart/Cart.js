d4c46efd4a913ff55245ac5114a992d583b40542
mehedi-hasan-saikat <m.h.saikat15@gmail.com>
2021-09-24 23:28:48 +0600

    first commit

--- src/Component/Header/Cart/Cart.js
+++ src/Component/Header/Cart/Cart.js
@@ -1,10 +1,17 @@
 import React from 'react';
 import './Cart.css'
 const Cart = (props) => {
+    const {cart}=props;
+    let total=0;
+    for(const product of cart)
+    {
+        total=total+product.price;
+    }
     return (
         <div>
            <h3>Order Summery</h3>
 <h4>Item Orderd: {props.cart.length}</h4> 
+<p>Total: {total}</p>
         </div>
     );
 };
